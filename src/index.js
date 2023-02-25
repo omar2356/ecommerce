1fb45b65f149758e124ef078e5638cbcf0c89f98
omar <alxomar235@gmail.com>
2023-02-25 14:42:04 +0300

    markh

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ import '@fortawesome/fontawesome-free/js/all.min'
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.add-to-cart-btn').click(function(){
+        alert("أضيف المُنتج إلى عربة الشراء");
+    });
 });
