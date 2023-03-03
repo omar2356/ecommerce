c139596c075e6dad30da9074d50cf61caf2aa103
omar <alxomar235@gmail.com>
2023-03-03 07:53:50 +0300

    updet index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import '@fortawesome/fontawesome-free/js/all.min'
+import { data } from 'jquery';
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('.add-to-cart-btn').click(function(){
         alert("أضيف المُنتج إلى عربة الشراء");
     });
+
+    $("#copyright").text(" جميع الحقوق محفوظة للمتجر سنة "   + new Date().getFullYear());
 });
