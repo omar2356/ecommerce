02fa2b9b144b1fcf58c61c498ab7f99e6a62258b
omar <alxomar235@gmail.com>
2023-04-23 11:32:25 +0300

    sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import './scss/style.scss'
 import "./css/style.css";
 import "jquery/dist/jquery.min";
 import "popper.js/dist/popper.min";
