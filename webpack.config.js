45e7a94ae141d89fb9962f70c78a2ed1544f1b4e
omar <alxomar235@gmail.com>
2023-04-26 09:40:38 +0300

    nwe

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     output: {
 
       path: path.join(__dirname, '/dist'),  
-      publicPath: './',
+      publicPath: '',
       filename: 'main.js',
     },
     mode: 'development',
